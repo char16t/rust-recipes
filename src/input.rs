52c3ddce88361a90d6956c9a0e2ac5a83ef21044
Valeriy Manenkov <valeriy@manenkov.com>
2024-05-21 10:42:19 +0300

    input: fix warnings

--- src/input.rs
+++ src/input.rs
@@ -1,15 +1,33 @@
 use std::io;
 use std::io::BufRead;
+use std::str::SplitWhitespace;
 
-pub fn take_int() -> usize {
-    take_int_from_reader(std::io::stdin())
+#[allow(dead_code)]
+pub fn take_int() -> i64 {
+    _take_int_from_reader(std::io::stdin())
 }
 
-fn take_int_from_reader<R: io::Read>(reader: R) -> usize {
+#[allow(dead_code)]
+pub fn take_2int() -> (i64, i64) {
+    _take_2int_from_reader(std::io::stdin())
+}
+
+fn _take_int_from_reader<R: io::Read>(reader: R) -> i64 {
     let mut input: String = String::new();
     let mut reader: io::BufReader<R> = io::BufReader::new(reader);
     reader.read_line(&mut input).expect("Failed to read line");
     input.trim().parse().unwrap()
+}
+
+fn _take_2int_from_reader<R: io::Read>(reader: R) -> (i64, i64) {
+    let mut input: String = String::new();
+    let mut reader: io::BufReader<R> = io::BufReader::new(reader);
+    reader.read_line(&mut input).expect("Failed to read line");
+
+    let mut values: SplitWhitespace = input.trim().split_whitespace();
+    let ax: i64 = values.next().unwrap().parse().unwrap();
+    let ay: i64 = values.next().unwrap().parse().unwrap();
+    (ax, ay)
 }
 
 #[cfg(test)]
@@ -18,11 +36,16 @@ mod tests {
 
     #[test]
     fn test_take_int() {
-        // Simulate input for testing
         let input: &str = "42\n";
-        let result: usize = take_int_from_reader(input.as_bytes());
-    
-        // Check the output
+        let result: i64 = _take_int_from_reader(input.as_bytes());
         assert_eq!(result, 42);
+    }
+
+    #[test]
+    fn tcest_take_2int() {
+        let input: &str = "-12 100\n";
+        let (x, y): (i64, i64) = _take_2int_from_reader(input.as_bytes());
+        assert_eq!(x, -12);
+        assert_eq!(y, 100);
     }
 }
