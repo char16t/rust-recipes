261cd85b1eccfeda920c056dfbcf5943a4a7c1cf
Valeriy Manenkov <valeriy@manenkov.com>
2024-05-21 10:56:26 +0300

    input: generify functions

--- src/input.rs
+++ src/input.rs
@@ -1,32 +1,41 @@
 use std::io;
-use std::io::BufRead;
-use std::str::SplitWhitespace;
+use std::io::{Read, BufRead};
+use std::str::{SplitWhitespace, FromStr};
 
 #[allow(dead_code)]
-pub fn take_int() -> i64 {
+pub fn take_int<T>() -> T where T: FromStr, <T as FromStr>::Err: std::fmt::Debug {
     _take_int_from_reader(std::io::stdin())
 }
 
 #[allow(dead_code)]
-pub fn take_2int() -> (i64, i64) {
+pub fn take_2int<T1, T2>() -> (T1, T2) 
+where 
+    T1: FromStr, <T1 as FromStr>::Err: std::fmt::Debug, 
+    T2: FromStr, <T2 as FromStr>::Err: std::fmt::Debug
+{
     _take_2int_from_reader(std::io::stdin())
 }
 
-fn _take_int_from_reader<R: io::Read>(reader: R) -> i64 {
+fn _take_int_from_reader<T, R>(reader: R) -> T where T: FromStr, <T as FromStr>::Err: std::fmt::Debug, R: Read {
     let mut input: String = String::new();
     let mut reader: io::BufReader<R> = io::BufReader::new(reader);
     reader.read_line(&mut input).expect("Failed to read line");
     input.trim().parse().unwrap()
 }
 
-fn _take_2int_from_reader<R: io::Read>(reader: R) -> (i64, i64) {
+fn _take_2int_from_reader<T1, T2, R>(reader: R) -> (T1, T2)  
+where 
+    T1: FromStr, <T1 as FromStr>::Err: std::fmt::Debug, 
+    T2: FromStr, <T2 as FromStr>::Err: std::fmt::Debug, 
+    R: Read
+{
     let mut input: String = String::new();
     let mut reader: io::BufReader<R> = io::BufReader::new(reader);
     reader.read_line(&mut input).expect("Failed to read line");
 
     let mut values: SplitWhitespace = input.trim().split_whitespace();
-    let ax: i64 = values.next().unwrap().parse().unwrap();
-    let ay: i64 = values.next().unwrap().parse().unwrap();
+    let ax: T1 = values.next().unwrap().parse().unwrap();
+    let ay: T2 = values.next().unwrap().parse().unwrap();
     (ax, ay)
 }
 
@@ -42,7 +51,7 @@ mod tests {
     }
 
     #[test]
-    fn tcest_take_2int() {
+    fn test_take_2int() {
         let input: &str = "-12 100\n";
         let (x, y): (i64, i64) = _take_2int_from_reader(input.as_bytes());
         assert_eq!(x, -12);
