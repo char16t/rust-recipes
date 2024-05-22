ffe13080b5979cce5b6f8b7ef4cf1a33c980f5bc
Valeriy Manenkov <valeriy@manenkov.com>
2024-05-22 13:25:57 +0300

    ranges: prefix sum array 3D

--- src/ranges.rs
+++ src/ranges.rs
@@ -52,6 +52,21 @@ pub fn range_sum_2d<T>(arr: &[T], rows: usize, cols: usize, top_left: (usize, us
 where 
     T: Default + std::ops::Add<Output = T> + std::ops::Sub<Output = T> + Copy
 {
+    if top_left.0 > bottom_right.0 {
+        panic!(
+            "Coordinate X of top left point ({}, {}) should be less or equals of bottom right's coordinate X ({}, {})",
+            top_left.0, top_left.1,
+            bottom_right.0, bottom_right.1 
+        );
+    }
+    if top_left.1 > bottom_right.1 {
+        panic!(
+            "Coordinate Y of top left point ({}, {}) should be less or equals of bottom right's coordinate Y ({}, {})",
+            top_left.0, top_left.1,
+            bottom_right.0, bottom_right.1 
+        );
+    }
+
     // * +-----------+
     // * |  D     C  |
     // * |   +---+   |
@@ -76,6 +91,148 @@ where
         let c: usize = xy(top_left.0 - 1,  bottom_right.1);
         let d: usize = xy(top_left.0 - 1, top_left.1 - 1);
         return arr[a] - arr[b] - arr[c] + arr[d]
+    }
+}
+
+pub fn build_prefix_sum_array_3d<T>(arr: &[T], rows: usize, cols: usize, depth: usize) -> Vec<T>
+where 
+    T: Default + std::ops::Add<Output = T> + std::ops::Sub<Output = T> + Copy 
+{
+    let mut prefix_sum: Vec<T> = vec![T::default(); arr.len()];
+    let xy = coordinates::create_coordinate_function_3d!(rows, cols, depth);
+    
+    // Filling first row, first col, first depth
+    prefix_sum[xy(0, 0, 0)] = arr[xy(0, 0, 0)];
+    for i in 1..rows {
+        prefix_sum[xy(i, 0, 0)] = prefix_sum[xy(i - 1, 0, 0)] + arr[xy(i, 0, 0)]
+    }
+    for i in 1..cols {
+        prefix_sum[xy(0, i, 0)] = prefix_sum[xy(0, i-1, 0)] + arr[xy(0, i, 0)]
+    }
+    for i in 1..depth {
+        prefix_sum[xy(0, 0, i)] = prefix_sum[xy(0, 0, i-1)] + arr[xy(0, 0, i)]
+    }
+
+    // Filling the cells of sides
+    for j in 1..cols {
+        for k in 1..depth {
+            prefix_sum[xy(0, j, k)] = arr[xy(0, j, k)]
+                + prefix_sum[xy(0, j-1, k)]
+                + prefix_sum[xy(0, j, k-1)]
+                - prefix_sum[xy(0, j-1, k-1)]
+        }
+    }
+    for i in 1..rows {
+        for k in 1..depth {
+            prefix_sum[xy(i, 0, k)] = arr[xy(i, 0, k)]
+                + prefix_sum[xy(i-1, 0, k)]
+                + prefix_sum[xy(i, 0, k-1)]
+                - prefix_sum[xy(i-1, 0, k-1)]
+        }
+    }
+    for i in 1..rows {
+        for j in 1..cols {
+            prefix_sum[xy(i, j, 0)] = arr[xy(i, j, 0)]
+                + prefix_sum[xy(i-1, j, 0)]
+                + prefix_sum[xy(i, j-1, 0)]
+                - prefix_sum[xy(i-1, j-1, 0)]
+        }
+    }
+
+    // Fill rest
+    for i in 1..rows {
+        for j in 1..cols {
+            for k in 1..depth {
+                prefix_sum[xy(i, j, k)] = arr[xy(i, j, k)]
+                    + prefix_sum[xy(i-1, j, k)]
+                    + prefix_sum[xy(i, j-1, k)]
+                    + prefix_sum[xy(i, j, k-1)]
+                    - prefix_sum[xy(i-1, j-1, k)]
+                    - prefix_sum[xy(i, j-1, k-1)]
+                    - prefix_sum[xy(i-1, j, k-1)]
+                    + prefix_sum[xy(i-1, k-1, j-1)] 
+            }
+        }
+    }
+    prefix_sum
+}
+
+///
+/// Point: (x = rows, y = cols, z = depth)
+/// 
+/// TL - top left
+/// BR - bottom right
+/// 
+//        +---------+
+//       /|        /|
+//      / |       / |
+//  TL •--|------+  |
+//     |  +------|--• BR 
+//     | /       | /
+//     |/        |/
+//     +--------+
+//
+pub fn range_sum_3d<T>(arr: &[T], rows: usize, cols: usize, depth: usize, top_left: (usize, usize, usize), bottom_right: (usize, usize, usize)) -> T 
+where 
+    T: Default + std::ops::Add<Output = T> + std::ops::Sub<Output = T> + Copy
+{
+    if top_left.0 > bottom_right.0 {
+        panic!(
+            "Coordinate X of top left point ({}, {}, {}) should be less or equals of bottom right's coordinate X ({}, {}, {})",
+            top_left.0, top_left.1, top_left.2,
+            bottom_right.0, bottom_right.1, bottom_right.2
+        );
+    }
+    if top_left.1 > bottom_right.1 {
+        panic!(
+            "Coordinate Y of top left point ({}, {}, {}) should be less or equals of bottom right's coordinate Y ({}, {}, {})",
+            top_left.0, top_left.1, top_left.2,
+            bottom_right.0, bottom_right.1, bottom_right.2
+        );
+    }
+    if top_left.2 > bottom_right.2 {
+        panic!(
+            "Coordinate Z of top left point ({}, {}, {}) should be less or equals of bottom right's coordinate Z ({}, {}, {})",
+            top_left.0, top_left.1, top_left.2,
+            bottom_right.0, bottom_right.1, bottom_right.2
+        );
+    }
+
+    let xy = coordinates::create_coordinate_function_3d!(rows, cols, depth);
+    let (x1, y1, z1): (usize, usize, usize) = top_left;
+    let (x2, y2, z2): (usize, usize, usize) = bottom_right;
+    if x1 == 0 && y1 == 0 && z1 == 0 {
+        return arr[xy(x2, y2, z2)];
+    } else if x1 == 0 && y1 == 0 {
+        return arr[xy(x2, y2, z2)] - arr[xy(x2, y2, z1-1)]
+    } else if x1 == 0 && z1 == 0 {
+        return arr[xy(x2, y2, z2)] - arr[xy(x2, y1-1, z2)]; 
+    } else if y1 == 0 && z1 == 0 {
+        return arr[xy(x2, y2, z2)] - arr[xy(x1-1, y2, z2)];
+    } else if x1 == 0 {
+        return arr[xy(x2, y2, z2)] 
+            - arr[xy(x2, y1-1, z2)] 
+            - arr[xy(x2, y2, z1-1)]
+            + arr[xy(x2, y1-1, z1-1)];
+    } else if y1 == 0 {
+        return arr[xy(x2, y2, z2)] 
+            - arr[xy(x1-1, y2, z2)] 
+            - arr[xy(x2, y2, z1-1)]
+            + arr[xy(x1-1, y2, z1-1)];
+    } else if z1 == 0 {
+        return arr[xy(x2, y2, z2)] 
+            - arr[xy(x1-1, y2, z2)] 
+            - arr[xy(x2, y1-1, z2)] 
+            + arr[xy(x1-1, y1-1, z2)];
+    } else {
+        return arr[xy(x2, y2, z2)] 
+            - arr[xy(x1-1, y2, z2)] 
+            - arr[xy(x2, y1-1, z2)] 
+            - arr[xy(x2, y2, z1-1)]
+            + arr[xy(x1-1, y1-1, z2)] 
+            + arr[xy(x1-1, y2, z1-1)] 
+            + arr[xy(x2, y1-1, z1-1)] 
+            - arr[xy(x1-1, y1-1, z1-1)];
     }
 }
 
@@ -168,5 +325,292 @@ mod tests {
 
         let sum: i32 = range_sum_2d(&prefix_sum_array, 4, 5, (0, 1), (2, 3));
         assert_eq!(sum, 9);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_range_sum_2d_panic_x() {
+        let original_array: Vec<i32> = vec![
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+        ];
+        let prefix_sum_array: Vec<i32> = build_prefix_sum_array_2d(&original_array, 4, 5);
+
+        range_sum_2d(&prefix_sum_array, 4, 5, (2, 3), (1, 1));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_range_sum_2d_panic_y() {
+        let original_array: Vec<i32> = vec![
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1,
+        ];
+        let prefix_sum_array: Vec<i32> = build_prefix_sum_array_2d(&original_array, 4, 5);
+
+        range_sum_2d(&prefix_sum_array, 4, 5, (1, 3), (2, 2));
+    }
+
+    #[test]
+    fn test_build_prefix_sum_array_3d() {
+        let original_array: Vec<i32> = vec![
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+        ];
+        let prefix_sum_array: Vec<i32> = build_prefix_sum_array_3d(&original_array, 4, 4, 4);
+        /*
+            1 2 3 4 
+            2 4 6 8 
+            3 6 9 12 
+            4 8 12 16 
+
+            2 4 6 8 
+            4 8 12 16 
+            6 12 18 24 
+            8 16 24 32
+
+            3 6 9 12 
+            6 12 18 24 
+            9 18 27 36 
+            12 24 36 48 
+
+            4 8 12 16 
+            8 16 24 32 
+            12 24 36 48 
+            16 32 48 64 
+        */
+        assert_eq!(prefix_sum_array.len(), 64);
+        
+        assert_eq!(prefix_sum_array[0], 1);
+        assert_eq!(prefix_sum_array[1], 2);
+        assert_eq!(prefix_sum_array[2], 3);
+        assert_eq!(prefix_sum_array[3], 4);
+        assert_eq!(prefix_sum_array[4], 2);
+        assert_eq!(prefix_sum_array[5], 4);
+        assert_eq!(prefix_sum_array[6], 6);
+        assert_eq!(prefix_sum_array[7], 8);
+        assert_eq!(prefix_sum_array[8], 3);
+        assert_eq!(prefix_sum_array[9], 6);
+        assert_eq!(prefix_sum_array[10], 9);
+        assert_eq!(prefix_sum_array[11], 12);
+        assert_eq!(prefix_sum_array[12], 4);
+        assert_eq!(prefix_sum_array[13], 8);
+        assert_eq!(prefix_sum_array[14], 12);
+        assert_eq!(prefix_sum_array[15], 16);
+
+        assert_eq!(prefix_sum_array[16], 2);
+        assert_eq!(prefix_sum_array[17], 4);
+        assert_eq!(prefix_sum_array[18], 6);
+        assert_eq!(prefix_sum_array[19], 8);
+        assert_eq!(prefix_sum_array[20], 4);
+        assert_eq!(prefix_sum_array[21], 8);
+        assert_eq!(prefix_sum_array[22], 12);
+        assert_eq!(prefix_sum_array[23], 16);
+        assert_eq!(prefix_sum_array[24], 6);
+        assert_eq!(prefix_sum_array[25], 12);
+        assert_eq!(prefix_sum_array[26], 18);
+        assert_eq!(prefix_sum_array[27], 24);
+        assert_eq!(prefix_sum_array[28], 8);
+        assert_eq!(prefix_sum_array[29], 16);
+        assert_eq!(prefix_sum_array[30], 24);
+        assert_eq!(prefix_sum_array[31], 32);
+
+        assert_eq!(prefix_sum_array[32], 3);
+        assert_eq!(prefix_sum_array[33], 6);
+        assert_eq!(prefix_sum_array[34], 9);
+        assert_eq!(prefix_sum_array[35], 12);
+        assert_eq!(prefix_sum_array[36], 6);
+        assert_eq!(prefix_sum_array[37], 12);
+        assert_eq!(prefix_sum_array[38], 18);
+        assert_eq!(prefix_sum_array[39], 24);
+        assert_eq!(prefix_sum_array[40], 9);
+        assert_eq!(prefix_sum_array[41], 18);
+        assert_eq!(prefix_sum_array[42], 27);
+        assert_eq!(prefix_sum_array[43], 36);
+        assert_eq!(prefix_sum_array[44], 12);
+        assert_eq!(prefix_sum_array[45], 24);
+        assert_eq!(prefix_sum_array[46], 36);
+        assert_eq!(prefix_sum_array[47], 48);
+
+        assert_eq!(prefix_sum_array[48], 4);
+        assert_eq!(prefix_sum_array[49], 8);
+        assert_eq!(prefix_sum_array[50], 12);
+        assert_eq!(prefix_sum_array[51], 16);
+        assert_eq!(prefix_sum_array[52], 8);
+        assert_eq!(prefix_sum_array[53], 16);
+        assert_eq!(prefix_sum_array[54], 24);
+        assert_eq!(prefix_sum_array[55], 32);
+        assert_eq!(prefix_sum_array[56], 12);
+        assert_eq!(prefix_sum_array[57], 24);
+        assert_eq!(prefix_sum_array[58], 36);
+        assert_eq!(prefix_sum_array[59], 48);
+        assert_eq!(prefix_sum_array[60], 16);
+        assert_eq!(prefix_sum_array[61], 32);
+        assert_eq!(prefix_sum_array[62], 48);
+        assert_eq!(prefix_sum_array[63], 64);
+    }
+
+    #[test]
+    fn test_range_sum_3d() {
+        let original_array: Vec<i32> = vec![
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+        ];
+        let prefix_sum_array: Vec<i32> = build_prefix_sum_array_3d(&original_array, 4, 4, 4);
+        
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (0, 0, 0), (0, 0, 0));
+        assert_eq!(sum, 1);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (0, 0, 1), (0, 0, 3));
+        assert_eq!(sum, 3);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (0, 1, 0), (0, 1, 3));
+        assert_eq!(sum, 4);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (1, 0, 0), (1, 1, 3));
+        assert_eq!(sum, 8);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (1, 1, 0), (1, 1, 3));
+        assert_eq!(sum, 4);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (1, 0, 1), (2, 0, 3));
+        assert_eq!(sum, 6);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (0, 1, 1), (2, 1, 3));
+        assert_eq!(sum, 9);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (1, 1, 1), (1, 1, 1));
+        assert_eq!(sum, 1);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (1, 1, 1), (3, 3, 3));
+        assert_eq!(sum, 27);
+
+        let sum: i32 = range_sum_3d(&prefix_sum_array, 4, 4, 4, (0, 0, 0), (3, 3, 3));
+        assert_eq!(sum, 64);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_range_sum_3d_panic_x() {
+        let original_array: Vec<i32> = vec![
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+        ];
+        let prefix_sum_array: Vec<i32> = build_prefix_sum_array_3d(&original_array, 4, 4, 4);
+        
+        range_sum_3d(&prefix_sum_array, 4, 4, 4, (1, 0, 0), (0, 0, 0));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_range_sum_3d_panic_y() {
+        let original_array: Vec<i32> = vec![
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+        ];
+        let prefix_sum_array: Vec<i32> = build_prefix_sum_array_3d(&original_array, 4, 4, 4);
+        
+        range_sum_3d(&prefix_sum_array, 4, 4, 4, (0, 1, 0), (0, 0, 0));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_range_sum_3d_panic_z() {
+        let original_array: Vec<i32> = vec![
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+            1, 1, 1, 1,
+        ];
+        let prefix_sum_array: Vec<i32> = build_prefix_sum_array_3d(&original_array, 4, 4, 4);
+        
+        range_sum_3d(&prefix_sum_array, 4, 4, 4, (0, 0, 1), (0, 0, 0));
     }
 }
