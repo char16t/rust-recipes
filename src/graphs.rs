41ecdd5b54dd61fb2f5cfb645257cd639cdeeaeb
Valeriy Manenkov <valeriy@manenkov.com>
2024-05-24 15:20:48 +0300

    graphs: weighted graphs

--- src/graphs.rs
+++ src/graphs.rs
@@ -29,6 +29,36 @@ where
     }
 }
 
+pub struct AdjacencyListWightedGraph<T, W> {
+    adjacency_list: HashMap<T, Vec<(T, W)>>,
+    is_directed: bool
+}
+
+impl<T, W> AdjacencyListWightedGraph<T, W>
+where
+    T: Copy + Eq + std::hash::Hash,
+    W: Copy
+{
+    pub fn new_undirected() -> Self {
+        Self { adjacency_list: HashMap::new(), is_directed: false }
+    }
+    pub fn new_directed() -> Self {
+        Self { adjacency_list: HashMap::new(), is_directed: true }
+    }
+    pub fn add_edge(&mut self, a: T, b: T, w: W) {
+        self.adjacency_list.entry(a).or_insert(Vec::new()).push((b, w));
+        if !self.is_directed {
+            self.adjacency_list.entry(b).or_insert(Vec::new()).push((a, w));
+        }
+    }
+    pub fn neighbors(&self, v: T) -> &[(T, W)] {
+        match self.adjacency_list.get(&v) {
+            Some(vec) => vec,
+            None => &[]
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,6 +113,59 @@ mod tests {
         assert_eq!(neighbors.len(), 0);
 
         let neighbors: &[i32] = graph.neighbors(4);
+        assert_eq!(neighbors.len(), 0);
+    }
+
+    #[test]
+    fn test_adjacency_list_weighed_undirected_graph() {
+        let mut graph: AdjacencyListWightedGraph<i32, f64> = AdjacencyListWightedGraph::new_undirected();
+        graph.add_edge(0, 1, 0.5);
+        graph.add_edge(0, 2, 0.7);
+        graph.add_edge(1, 3, 0.9);
+        graph.add_edge(2, 3, 0.1);
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(0);
+        assert_eq!(neighbors[0], (1, 0.5));
+        assert_eq!(neighbors[1], (2, 0.7));
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(1);
+        assert_eq!(neighbors[0], (0, 0.5));
+        assert_eq!(neighbors[1], (3, 0.9));
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(2);
+        assert_eq!(neighbors[0], (0, 0.7));
+        assert_eq!(neighbors[1], (3, 0.1));
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(3);
+        assert_eq!(neighbors[0], (1, 0.9));
+        assert_eq!(neighbors[1], (2, 0.1));
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(4);
+        assert_eq!(neighbors.len(), 0);
+    }
+
+    #[test]
+    fn test_adjacency_list_weighed_directed_graph() {
+        let mut graph: AdjacencyListWightedGraph<i32, f64> = AdjacencyListWightedGraph::new_directed();
+        graph.add_edge(0, 1, 0.5);
+        graph.add_edge(0, 2, 0.7);
+        graph.add_edge(1, 3, 0.9);
+        graph.add_edge(2, 3, 0.1);
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(0);
+        assert_eq!(neighbors[0], (1, 0.5));
+        assert_eq!(neighbors[1], (2, 0.7));
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(1);
+        assert_eq!(neighbors[0], (3, 0.9));
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(2);
+        assert_eq!(neighbors[0], (3, 0.1));
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(3);
+        assert_eq!(neighbors.len(), 0);
+
+        let neighbors: &[(i32, f64)] = graph.neighbors(4);
         assert_eq!(neighbors.len(), 0);
     }
 }
