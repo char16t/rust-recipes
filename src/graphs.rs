6bfb2fc863fb6d4e213efd6826b11fa0c84f1542
Valeriy Manenkov <valeriy@manenkov.com>
2024-05-24 14:58:07 +0300

    graphs: simple directed graph

--- src/graphs.rs
+++ src/graphs.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
-pub struct Graph<T> {
+pub struct UndirectedGraph<T> {
     adjacency_list: HashMap<T, Vec<T>>,
 }
 
-impl<T> Graph<T>
+impl<T> UndirectedGraph<T>
 where
     T: Copy + Eq + std::hash::Hash
 {
@@ -13,7 +13,29 @@ where
     }
     pub fn add_edge(&mut self, a: T, b: T) {
         self.adjacency_list.entry(a).or_insert(Vec::new()).push(b);
-        self.adjacency_list.entry(b).or_insert(Vec::new()).push(a); // For undirected graph
+        self.adjacency_list.entry(b).or_insert(Vec::new()).push(a);
+    }
+    pub fn neighbors(&self, v: T) -> &[T] {
+        match self.adjacency_list.get(&v) {
+            Some(vec) => vec,
+            None => &[]
+        }
+    }
+}
+
+pub struct DirectedGraph<T> {
+    adjacency_list: HashMap<T, Vec<T>>,
+}
+
+impl<T> DirectedGraph<T>
+where
+    T: Copy + Eq + std::hash::Hash
+{
+    pub fn new() -> Self {
+        Self { adjacency_list: HashMap::new() }
+    }
+    pub fn add_edge(&mut self, a: T, b: T) {
+        self.adjacency_list.entry(a).or_insert(Vec::new()).push(b);
     }
     pub fn neighbors(&self, v: T) -> &[T] {
         match self.adjacency_list.get(&v) {
@@ -28,8 +50,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_graph() {
-        let mut graph: Graph<i32> = Graph::new();
+    fn test_undirected_graph() {
+        let mut graph: UndirectedGraph<i32> = UndirectedGraph::new();
         graph.add_edge(0, 1);
         graph.add_edge(0, 2);
         graph.add_edge(1, 3);
@@ -50,6 +72,31 @@ mod tests {
         let neighbors: &[i32] = graph.neighbors(3);
         assert_eq!(neighbors[0], 1);
         assert_eq!(neighbors[1], 2);
+
+        let neighbors: &[i32] = graph.neighbors(4);
+        assert_eq!(neighbors.len(), 0);
+    }
+
+    #[test]
+    fn test_directed_graph() {
+        let mut graph: DirectedGraph<i32> = DirectedGraph::new();
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 2);
+        graph.add_edge(1, 3);
+        graph.add_edge(2, 3);
+
+        let neighbors: &[i32] = graph.neighbors(0);
+        assert_eq!(neighbors[0], 1);
+        assert_eq!(neighbors[1], 2);
+
+        let neighbors: &[i32] = graph.neighbors(1);
+        assert_eq!(neighbors[0], 3);
+
+        let neighbors: &[i32] = graph.neighbors(2);
+        assert_eq!(neighbors[0], 3);
+
+        let neighbors: &[i32] = graph.neighbors(3);
+        assert_eq!(neighbors.len(), 0);
 
         let neighbors: &[i32] = graph.neighbors(4);
         assert_eq!(neighbors.len(), 0);
