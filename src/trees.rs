a02bc4f1f115bd0bfaa6d6645655ade5e33dd04d
Valeriy Manenkov <valeriy@manenkov.com>
2024-06-05 18:22:52 +0300

    trees: calculate diameter for adjacency list tree

--- src/trees.rs
+++ src/trees.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[repr(transparent)]
 pub struct AdjacencyListTree<T> {
@@ -13,6 +13,7 @@ where
     }
     pub fn add_child(&mut self, parent: T, child: T) {
         self.adjacency_list.entry(parent).or_insert(Vec::new()).push(child);
+        self.adjacency_list.entry(child).or_insert(Vec::new()).push(parent);
     }
     pub fn iter_dfs(&self, start_node: T) -> AdjacencyListTreeDfsIterator<T> {
         AdjacencyListTreeDfsIterator::new(&self.adjacency_list, start_node)
@@ -20,12 +21,59 @@ where
     pub fn iter_bfs(&self, start_node: T) -> AdjacencyListTreeBfsIterator<T> {
         AdjacencyListTreeBfsIterator::new(&self.adjacency_list, start_node)
     }
+    pub fn diameter(&self) -> usize {
+        if let Some(start_node) = self.adjacency_list.keys().next() {
+            let v: &T = start_node;
+            let mut u: &T = start_node;
+            let mut w: &T = start_node;
+
+            let distances: HashMap<T, usize> = self.bfs_distances(v);
+            for i in self.adjacency_list.keys() {
+                if distances[i] > distances[u] {
+                    u = i;
+                }
+            }
+
+            let distances: HashMap<T, usize> = self.bfs_distances(u);
+            for i in self.adjacency_list.keys() {
+                if distances[i] > distances[w] {
+                    w = i;
+                }
+            }
+
+            return distances[w]
+        }
+        0
+    }
+
+    #[inline]
+    fn bfs_distances(&self, start: &T) -> HashMap<T, usize> {
+        let mut distances: HashMap<T, usize> = HashMap::new();
+        let mut visited: HashSet<T> = HashSet::new();
+        let mut queue: VecDeque<T> = VecDeque::new();
+    
+        queue.push_back(*start);
+        visited.insert(*start);
+        distances.insert(*start, 0);
+    
+        while let Some(node) = queue.pop_front() {
+            for &neighbor in self.adjacency_list.get(&node).unwrap_or(&vec![]) {
+                if !visited.contains(&neighbor) {
+                    visited.insert(neighbor);
+                    distances.insert(neighbor, distances[&node] + 1);
+                    queue.push_back(neighbor);
+                }
+            }
+        }
+    
+        distances
+    }
 }
 
 pub struct AdjacencyListTreeDfsIterator<'a, T> {
     adjacency_list: &'a HashMap<T, Vec<T>>,
     stack: VecDeque<T>,
-    previous: Option<T>
+    visited: HashSet<T>
 }
 impl<'a, T> AdjacencyListTreeDfsIterator<'a, T>
 where
@@ -33,12 +81,14 @@ where
 {
     fn new(adjacency_list: &'a HashMap<T, Vec<T>>, start_node: T) -> Self {
         let mut stack: VecDeque<T> = VecDeque::new();
+        let mut visited: HashSet<T> = HashSet::new();
         stack.push_front(start_node);
+        visited.insert(start_node);
 
         AdjacencyListTreeDfsIterator {
             adjacency_list,
             stack,
-            previous: None
+            visited
         }
     }
 }
@@ -50,16 +100,10 @@ where
 
     fn next(&mut self) -> Option<T> {
         if let Some(node) = self.stack.pop_front() {
-            if let Some(neighbors) = self.adjacency_list.get(&node) {
-                for &neighbor in neighbors {
-                    let allowed: bool = match self.previous {
-                        Some(prev) => prev != neighbor,
-                        None => true
-                    };
-                    if allowed {
-                        self.stack.push_front(neighbor);
-                        self.previous = Some(node);
-                    }
+            for &neighbor in &self.adjacency_list[&node] {
+                if !self.visited.contains(&neighbor) {
+                    self.stack.push_front(neighbor);
+                    self.visited.insert(neighbor);
                 }
             }
             return Some(node);
@@ -71,7 +115,7 @@ where
 pub struct AdjacencyListTreeBfsIterator<'a, T> {
     adjacency_list: &'a HashMap<T, Vec<T>>,
     queue: VecDeque<T>,
-    previous: Option<T>
+    visited: HashSet<T>,
 }
 impl<'a, T> AdjacencyListTreeBfsIterator<'a, T>
 where
@@ -79,12 +123,14 @@ where
 {
     fn new(adjacency_list: &'a HashMap<T, Vec<T>>, start_node: T) -> Self {
         let mut queue: VecDeque<T> = VecDeque::new();
+        let mut visited: HashSet<T> = HashSet::new();
         queue.push_back(start_node);
+        visited.insert(start_node);
 
         AdjacencyListTreeBfsIterator {
             adjacency_list,
             queue,
-            previous: None
+            visited,
         }
     }
 }
@@ -96,16 +142,10 @@ where
 
     fn next(&mut self) -> Option<T> {
         if let Some(node) = self.queue.pop_front() {
-            if let Some(neighbors) = self.adjacency_list.get(&node) {
-                for &neighbor in neighbors {
-                    let allowed: bool = match self.previous {
-                        Some(prev) => prev != neighbor,
-                        None => true
-                    };
-                    if allowed {
-                        self.queue.push_back(neighbor);
-                        self.previous = Some(node);
-                    }
+            for &neighbor in &self.adjacency_list[&node] {
+                if !self.visited.contains(&neighbor) {
+                    self.queue.push_back(neighbor);
+                    self.visited.insert(neighbor);
                 }
             }
             return Some(node);
@@ -233,6 +273,41 @@ mod tests {
         let actual: Vec<i32> = tree.iter_bfs(0).collect();
         let expected: Vec<i32> = vec![0, 1, 2, 3, 11, 12, 13, 21, 22, 23, 31, 32, 33, 221, 222, 223];
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_adjacency_list_tree_diameter() {
+        let tree: AdjacencyListTree<i32> = AdjacencyListTree::new();
+        let diameter: usize = tree.diameter();
+        assert_eq!(diameter, 0);
+
+        let mut tree: AdjacencyListTree<i32> = AdjacencyListTree::new();
+        tree.add_child(0, 1);
+        let diameter: usize = tree.diameter();
+        assert_eq!(diameter, 1);
+
+        let mut tree: AdjacencyListTree<i32> = AdjacencyListTree::new();
+        tree.add_child(0, 1);
+        tree.add_child(0, 2);
+        let diameter: usize = tree.diameter();
+        assert_eq!(diameter, 2);
+
+        let mut tree: AdjacencyListTree<i32> = AdjacencyListTree::new();
+        tree.add_child(0, 1);
+        tree.add_child(0, 2);
+        tree.add_child(0, 3);
+        let diameter: usize = tree.diameter();
+        assert_eq!(diameter, 2);
+
+        let mut tree: AdjacencyListTree<i32> = AdjacencyListTree::new();
+        tree.add_child(0, 1);
+        tree.add_child(0, 2);
+        tree.add_child(2, 4);
+        tree.add_child(4, 5);
+        tree.add_child(0, 3);
+        tree.add_child(3, 6);
+        let diameter: usize = tree.diameter();
+        assert_eq!(diameter, 5);
     }
 
     #[test]
