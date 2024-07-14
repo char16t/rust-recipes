3725a8a929c75220c21c04d1016872fe4f911d60
Valeriy Manenkov <valeriy@manenkov.com>
2024-07-14 10:04:59 +0300

    matrices: diagonal matrices and access by indices

--- src/matrices.rs
+++ src/matrices.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+use std::ops::IndexMut;
 use std::ops::Add;
 use std::ops::Mul;
 
@@ -35,6 +37,33 @@ where
             data
         }
     }
+    pub fn new_diag(size: usize, elem: T) -> Self {
+        let mut data: Vec<T> = vec![T::default(); size * size];
+        for i in 0..size {
+            data[i * size + i] = elem;
+        }
+        Self {
+            rows: size, 
+            cols: size, 
+            data
+        }
+    }
+}
+
+impl<T> Index<usize> for Matrix<T> {
+    type Output = [T];
+
+    fn index(&self, index: usize) -> &Self::Output {
+        let start: usize = index * self.cols;
+        &self.data[start..start + self.cols]
+    }
+}
+
+impl<T> IndexMut<usize> for Matrix<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        let start: usize = index * self.cols;
+        &mut self.data[start..start + self.cols]
+    }
 }
 
 #[cfg(test)]
@@ -70,5 +99,44 @@ mod tests {
         assert_eq!(m.data[0], 10);
         assert_eq!(m.data[1], 20);
         assert_eq!(m.data[2], 30);
+    }
+
+    #[test]
+    fn test_matrix_diag() {
+        let m: Matrix<usize> = Matrix::new_diag(3, 1);
+        assert_eq!(m.rows, 3);
+        assert_eq!(m.cols, 3);
+        assert_eq!(m[0][0], 1);
+        assert_eq!(m[0][1], 0);
+        assert_eq!(m[0][2], 0);
+        assert_eq!(m[1][0], 0);
+        assert_eq!(m[1][1], 1);
+        assert_eq!(m[1][2], 0);
+        assert_eq!(m[2][0], 0);
+        assert_eq!(m[2][1], 0);
+        assert_eq!(m[2][2], 1);
+
+        let m: Matrix<usize> = Matrix::new_diag(1, 1);
+        assert_eq!(m[0][0], 1);
+    }
+
+    #[test]
+    fn test_matrix_data_access() {
+        let mut m: Matrix<usize> = Matrix::new(2, 3);
+        m[0][0] = 1;
+        m[0][1] = 2;
+        m[0][2] = 3;
+        m[1][0] = 4;
+        m[1][1] = 5;
+        m[1][2] = 6;
+        assert_eq!(m.data, vec![1, 2, 3, 4, 5, 6]);
+
+        assert_eq!(m[0][0], 1);
+        assert_eq!(m[0][0], 1);
+        assert_eq!(m[0][1], 2);
+        assert_eq!(m[0][2], 3);
+        assert_eq!(m[1][0], 4);
+        assert_eq!(m[1][1], 5);
+        assert_eq!(m[1][2], 6);
     }
 }
