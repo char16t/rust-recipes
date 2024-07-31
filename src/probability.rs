f4f65d6ee350ee0003c46a07af4a1f39f9d8edc4
Valeriy Manenkov <valeriy@manenkov.com>
2024-07-31 08:48:25 +0300

    probability: generate random number in given distribution

--- src/probability.rs
+++ src/probability.rs
@@ -1,11 +1,13 @@
+use crate::random;
 use std::collections::HashMap;
 
 pub struct DiscreteRandomVariable {
+    rng: random::Xoshiro256,
     distribution: HashMap<i64, f64>
 }
 impl DiscreteRandomVariable {
     pub fn new() -> Self {
-        DiscreteRandomVariable { distribution: HashMap::new() }
+        DiscreteRandomVariable { distribution: HashMap::new(), rng: random::Xoshiro256::new() }
     }
     pub fn add(&mut self, value: i64, probability: f64) {
         self.distribution.insert(value, probability);
@@ -16,6 +18,20 @@ impl DiscreteRandomVariable {
             expected_value += probability * (value as f64)
         }
         expected_value
+    }
+    pub fn rand(&mut self) -> i64 {
+        let rand_num: f64 = self.rng.rand_float();
+    
+        let mut cumulative_prob: f64 = 0.0;
+        for (value, &prob) in &self.distribution {
+            cumulative_prob += prob;
+            if cumulative_prob >= rand_num {
+                return *value;
+            }
+        }
+    
+        // Return last key if for-loop ended without returning
+        *self.distribution.keys().last().unwrap()
     }
 }
 
@@ -36,5 +52,36 @@ mod tests {
         
         let ev: f64 = cube.expected_value();
         assert!(numbers::approx_equal(ev, 3.5, 0.00001));        
+    }
+
+    #[test]
+    fn test_discrete_random_variable_rand() {
+        let mut random_variable: DiscreteRandomVariable = DiscreteRandomVariable::new();       
+        random_variable.add(1, 0.8);
+        random_variable.add(2, 0.1);
+        random_variable.add(3, 0.1);
+
+        let n: i32 = 10000;
+        let mut count_map: HashMap<i64, usize> = random_variable.distribution.keys().map(|&k| (k, 0)).collect();
+        for _ in 0..n {
+            let random_value: i64 = random_variable.rand();
+            *count_map.entry(random_value).or_insert(0) += 1;
+        }
+    
+        let mut mean_absolute_percentage_error_sum: f64 = 0.0;
+
+        for (value, &count) in &count_map {
+            let expected_count: usize = (n as f64 * random_variable.distribution[&value]).round() as usize;
+            // println!("Value: {}, Expected Count: {}, Actual Count: {}", value, expected_count, count);
+
+            let count_difference: usize = expected_count.abs_diff(count);
+            mean_absolute_percentage_error_sum += count_difference as f64 / count as f64;
+        }
+
+        let mean_absolute_percentage_error: f64 = mean_absolute_percentage_error_sum as f64 / random_variable.distribution.len() as f64;
+        // println!("MAPE is {}", mean_absolute_percentage_error);
+
+        assert!(mean_absolute_percentage_error < 0.04, "MAPE (Mean Absolute Percentage Error): {} should be < 4%", mean_absolute_percentage_error);
+
     }
 }
