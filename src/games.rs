a8dacdb8b98825a54be287655158582c47e13151
Valeriy Manenkov <valeriy@manenkov.com>
2024-08-04 11:06:58 +0300

    games: grundy game

--- src/games.rs
+++ src/games.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn nim_sum<T>(elements: &[T]) -> T
 where
     T: Default + Copy + std::ops::BitXor<Output = T>
@@ -107,6 +109,84 @@ pub fn misere_nim_move(elements: &[usize]) -> (usize, usize) {
     }
 }
 
+pub fn mex(states: &[usize]) -> usize {
+    let mut grundy_numbers: HashSet<usize> = HashSet::new();
+    for &state in states {
+        grundy_numbers.insert(state);
+    }
+    let mut mex: usize = 0;
+    while grundy_numbers.contains(&mex) {
+        mex += 1;
+    }
+    return mex
+}
+
+pub fn is_losing_state(grundy_number: usize) -> bool {
+    grundy_number == 0
+}
+
+pub fn is_winning_state(grundy_number: usize) -> bool {
+    grundy_number != 0
+}
+
+pub fn grundy_number(n: usize) -> usize {
+    if n == 1 || n == 2 {
+        return 0;
+    }
+    let mut grundy_numbers: Vec<usize> = vec![];
+    let mut used_nums: HashSet<usize> = HashSet::new();
+    for i in 1..n {
+        if n - i != i && !used_nums.contains(&i) {
+            grundy_numbers.push(
+                nim_sum(&vec![grundy_number(i), grundy_number(n-i)])
+            );
+            used_nums.insert(i);
+            used_nums.insert(n-i);
+        }
+    }
+    return mex(&grundy_numbers);
+}
+
+/// Return (A, B, C). It means "Split A-th heap into B and C"
+pub fn grundy_game_move(heaps: &[usize]) -> (usize, usize, usize) {
+    
+    let mut heap_with_more_than_1_element: usize = 0;
+    let mut heap_with_more_than_1_element_exists: bool = false;
+    
+    for heap_index in 0..heaps.len() {
+        let n: usize = heaps[heap_index];
+        
+        if n > 1 {
+            heap_with_more_than_1_element = heap_index;
+            heap_with_more_than_1_element_exists = true;
+        }
+
+        let mut used_nums: HashSet<usize> = HashSet::new();
+        for i in 1..n {
+            if n - i != i && !used_nums.contains(&i) {
+                if nim_sum(&vec![grundy_number(i), grundy_number(n-i)]) == 0 {
+                    return (heap_index, i, n-i);
+                }
+                used_nums.insert(i);
+                used_nums.insert(n-i);
+            }
+        }
+    }
+
+    if heap_with_more_than_1_element_exists {
+        return (heap_with_more_than_1_element, 1, heaps[heap_with_more_than_1_element] - 1);
+    }
+    panic!("Unable to make move when heaps not exists, {:?}", heaps);
+}
+
+pub fn is_grundy_game_over(elements: &[usize]) -> bool {
+    for i in 0..elements.len() {
+        if elements[i] != 1 {
+            return false;
+        }
+    }
+    return true;
+}
 
 #[cfg(test)]
 mod tests {
@@ -184,5 +264,77 @@ mod tests {
         }
         assert_eq!(elements, vec![0, 0, 0]);
         assert_eq!(player, true);
+    }
+
+    #[test]
+    fn test_mex() {
+        assert_eq!(mex(&vec![0, 1, 3]), 2);
+    }
+
+    #[test]
+    fn test_is_losing_state() {
+        assert_eq!(is_losing_state(0), true);
+        assert_eq!(is_losing_state(5), false);
+    }
+
+    #[test]
+    fn test_is_winning_state() {
+        assert_eq!(is_winning_state(0), false);
+        assert_eq!(is_winning_state(5), true);
+    }
+
+    #[test]
+    fn test_grundy_number() {
+        assert_eq!(grundy_number(1), 0);
+        assert_eq!(grundy_number(2), 0);
+        assert_eq!(grundy_number(3), 1);
+        assert_eq!(grundy_number(4), 0);
+        assert_eq!(grundy_number(5), 2);
+        assert_eq!(grundy_number(6), 1);
+        assert_eq!(grundy_number(7), 0);
+        assert_eq!(grundy_number(8), 2);
+    }
+
+    #[test]
+    fn test_grundy_game_move() {
+        assert_eq!(grundy_game_move(&vec![8]), (0, 1, 7));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_grundy_game_move_panic() {
+        grundy_game_move(&vec![]);
+    }
+
+
+    #[test]
+    fn test_is_grundy_game_over() {
+        assert_eq!(is_grundy_game_over(&vec![]), true);
+        assert_eq!(is_grundy_game_over(&vec![10, 3]), false);
+        assert_eq!(is_grundy_game_over(&vec![1, 3, 1]), false);
+        assert_eq!(is_grundy_game_over(&vec![1, 1]), true);
+        assert_eq!(is_grundy_game_over(&vec![1]), true);
+    }
+
+    #[test]
+    fn test_grundy_game_integration() {
+        let mut player: bool = true;
+        
+        let mut heaps: Vec<usize> = vec![8];
+        while !is_grundy_game_over(&heaps) {
+            let p: &str = if player { "COMPUTER" } else { "HUMAN" };
+            println!("[{}] Elements: {:?}", p, heaps);
+            
+            let (heap_index, size_a, size_b) = grundy_game_move(&heaps);
+            heaps[heap_index] = size_a;
+            heaps.push(size_b);
+
+            println!("[{}] Split: {}-th heap into {} and {}", p, heap_index, size_a, size_b);
+            println!("[{}] Elements: {:?}", p, heaps);
+            player = !player;
+        }
+
+        assert_eq!(heaps, vec![1, 1, 1, 1, 1, 1, 1, 1]);
+        assert_eq!(player, false);
     }
 }
