e9f06acd4d3fe91ff3348938da587c6e66f7989f
Valeriy Manenkov <valeriy@manenkov.com>
2024-08-06 07:59:56 +0300

    geometry: cos

--- src/geometry.rs
+++ src/geometry.rs
@@ -12,6 +12,18 @@ pub fn sin<T: Into<f64> + Copy>(x: T) -> f64 {
     result
 }
 
+/// Approximates the cosine of an angle using the Taylor series expansion
+pub fn cos<T: Into<f64> + Copy>(x: T) -> f64 {
+    let x = x.into();
+    let mut result: f64 = 0.0;
+    for n in 0..16 {
+        let sign: f64 = if n % 2 == 0 { 1.0 } else { -1.0 };
+        let term = sign * x.powi(2 * n) / combinatorics::factorial_i128((2 * n).into()) as f64;
+        result += term;
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +35,13 @@ mod tests {
         assert!(numbers::approx_equal(sin(std::f64::consts::PI / 2.0), 1.0, 0.0000001));
         assert!(numbers::approx_equal(sin(std::f64::consts::PI / 4.0), 0.70710678119, 0.0000001));
         assert!(numbers::approx_equal(sin(std::f64::consts::PI * 2.0), 0.0, 0.0000001));
+    }
+
+    #[test]
+    fn test_cos() {
+        assert!(numbers::approx_equal(cos(std::f64::consts::PI), -1.0, 0.0000001));
+        assert!(numbers::approx_equal(cos(std::f64::consts::PI / 2.0), 0.0, 0.0000001));
+        assert!(numbers::approx_equal(cos(std::f64::consts::PI / 4.0), 0.70710678119, 0.0000001));
+        assert!(numbers::approx_equal(cos(std::f64::consts::PI * 2.0), 1.0, 0.0000001));
     }
 }
