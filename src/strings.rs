332d4413d4da367a46b2d507c1e45b93eac91351
Valeriy Manenkov <valeriy@manenkov.com>
2024-08-22 09:28:54 +0300

    strings: trie

--- src/strings.rs
+++ src/strings.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{combinatorics, graphs::AdjacencyListGraph};
 
 pub fn de_bruijn_sequence(alphabet: &[char], length: usize) -> String {
@@ -21,6 +23,46 @@ pub fn de_bruijn_sequence(alphabet: &[char], length: usize) -> String {
     }
 }
 
+#[derive(Default, Debug)]
+struct TrieNode {
+    is_end_of_word: bool,
+    children: HashMap<char, TrieNode>,
+}
+
+#[derive(Default, Debug)]
+pub struct Trie {
+    root: TrieNode,
+}
+
+impl Trie {
+    pub fn new() -> Self {
+        Trie {
+            root: TrieNode::default(),
+        }
+    }
+
+    pub fn insert(&mut self, word: &str) {
+        let mut current_node: &mut TrieNode = &mut self.root;
+
+        for c in word.chars() {
+            current_node = current_node.children.entry(c).or_default();
+        }
+        current_node.is_end_of_word = true;
+    }
+
+    pub fn contains(&self, word: &str) -> bool {
+        let mut current_node = &self.root;
+
+        for c in word.chars() {
+            match current_node.children.get(&c) {
+                Some(node) => current_node = node,
+                None => return false,
+            }
+        }
+
+        current_node.is_end_of_word
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -49,5 +91,29 @@ mod tests {
         let _k: usize = alphabet.len();
         let seq: String = de_bruijn_sequence(&alphabet, n);
         assert_eq!(seq, "");
+    }
+
+    #[test]
+    fn test_trie() {
+        let w_latin: &str = "alphabet";
+        let w_cyrillic: &str = "алфавит";
+        let w_emoji: &str = "🙂🙃😌🥲😁";
+
+        let mut trie: Trie = Trie::new();
+        trie.insert(w_latin);
+        trie.insert(w_cyrillic);
+        trie.insert(w_emoji);
+
+        assert!(trie.contains(w_latin));
+        assert!(!trie.contains("alpha"));
+        assert!(!trie.contains("alphabetical"));
+
+        assert!(trie.contains(w_cyrillic));
+        assert!(!trie.contains("алфа"));
+        assert!(!trie.contains("алфавитный"));
+
+        assert!(trie.contains(w_emoji));
+        assert!(!trie.contains("🙂🙃"));
+        assert!(!trie.contains("🙂🙃😌🥲😁😡🤢🌚"));
     }
 }
